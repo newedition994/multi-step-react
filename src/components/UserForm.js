129141d1e6befe196738ee94ebae59765ac07867
Breyanna Vass <breyannavass@gmail.com>
2019-07-23 11:54:22 -0400

    Adding state to UserForm

---
+++ src/components/UserForm.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+
+export class UserForm extends Component {
+
+    state = {
+        step: 1,
+        firstName: '',
+        lastName: '',
+        email: '',
+        occupation: '',
+        city: '',
+        bio: ''
+    }
+
+    // Proceed to next step
+
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default UserForm
