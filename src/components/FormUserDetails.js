fbc26aa24a2a67d3ec7f5eabe7d39aa7f433958a
Breyanna Vass <breyannavass@gmail.com>
2019-07-23 12:31:49 -0400

    Initial User details

--- src/components/FormUserDetails.js
+++ src/components/FormUserDetails.js
@@ -1,11 +1,49 @@
 import React, { Component } from 'react'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AppBar from 'material-ui/AppBar';
+import TextField from 'material-ui/TextField';
+import RaisedButton from 'material-ui/RaisedButton';
 
 export class FormUserDetails extends Component {
+
+    continue = e => {
+        e.preventDefault();
+        this.props.nextStep();
+    }
+
     render() {
+        const { values, handleChange } = this.props;
         return (
-            <div>
-                <h1>Hello from user details</h1>
-            </div>
+            <MuiThemeProvider>
+                <React.Fragment>
+                    <AppBar title="Enter User Details" />
+                    <TextField
+                        hintText="Enter Your First Name"
+                        floatingLabelText="First Name"
+                        onChange={handleChange('firstName')}
+                        defaultValue={values.firstName}
+                    />
+                    <br />
+                    <TextField
+                        hintText="Enter Your Last Name"
+                        floatingLabelText="Last Name"
+                        onChange={handleChange('lastName')}
+                        defaultValue={values.lastName}
+                    />
+                    <br />
+                    <TextField
+                        hintText="Enter Your Email"
+                        floatingLabelText="Email"
+                        onChange={handleChange('email')}
+                        defaultValue={values.email}
+                    />
+                    <br />
+                    <RaisedButton
+                        label="Continue"
+                        primary={true}
+                        style={StyleSheet.button} />
+                </React.Fragment>
+            </MuiThemeProvider>
         )
     }
 }
