2339ffc4ff063505ad99a5ddb102d430608817aa
Breyanna Vass <breyannavass@gmail.com>
2019-07-23 12:12:37 -0400

    Adding User form details

---
+++ src/components/FormUserDetails.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class FormUserDetails extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello from user details</h1>
+            </div>
+        )
+    }
+}
+
+export default FormUserDetails
--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import FormUserDetails from './FormUserDetails';
 
 export class UserForm extends Component {
 
@@ -35,11 +36,28 @@ export class UserForm extends Component {
 
 
     render() {
-        return (
-            <div>
 
-            </div>
-        )
+        const { step } = this.state;
+        const { firstName, lastName, email, occupation, city, bio } = this.state;
+        const values = { firstName, lastName, email, occupation, city, bio }
+
+        // eslint-disable-next-line default-case
+        switch (step) {
+            case 1:
+                return (
+                    <FormUserDetails
+                        nextStep={this.nextStep}
+                        handleChange={this.handleChange}
+                        values={values}
+                    />
+                )
+            case 2:
+                return <h1>FormPersonalDetails</h1>
+            case 3:
+                return <h1>Confirm</h1>
+            case 4:
+                return <h1>Success</h1>
+        }
     }
 }
 
